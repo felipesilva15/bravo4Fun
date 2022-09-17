5c3f6dbe651799cb18d1cae4a5397d5a533566b0
FELIPE DA SILVA SANTOS <felipe.ssantos80@senacsp.edu.br>
2022-09-16 22:17:14 -0300

    Novo modelo para tela de consulta

--- res/js/api.js
+++ res/js/api.js
@@ -1,11 +1,11 @@
 const api = {};
 
 // Realiza uma requisição de um arquivo do projeto sem retornar um conjunto de dados
-api.request = (rota, data) => {
+api.request = (rota, method, data) => {
 
 };
 
 // Realiza uma requisição de um arquivo do projeto retornando um conjunto de dados
-api.consultar = (rota, data) => {
+api.consultar = (rota, method, data) => {
 
 };
