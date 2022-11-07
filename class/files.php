5a3102a13a97fe78bc7aae186c3099c231c2c039
Felipe.Silva <felipe.allware@gmail.com>
2022-11-06 22:57:25 -0300

    Finalizando download e adicionando comentários no código

--- class/files.php
+++ class/files.php
@@ -1,25 +1,25 @@
 <?php
 
 class Files {
-    private $file, $dirFile, $filePath, $imgurClientID, $uploadType;
+    private $file, $fileDir, $filePath, $imgurClientID, $uploadType;
 
-    public function __construct($dirFile = ""){
-        if($dirFile){
-            $this->setDirFile($dirFile);
+    public function __construct($fileDir = ""){
+        if($fileDir){
+            $this->setFileDir($fileDir);
         } else{
-            $this->setDirFile("assets");
+            $this->setFileDir("assets");
         }
 
         $this->setImgurClientID("e27d6f9aee55c39");
         $this->setUploadType(1);
     }
 
-    public function setDirFile($dirFile){
-        $this->dirFile = $dirFile;
+    public function setFileDir($fileDir){
+        $this->fileDir = $fileDir;
     }
 
-    public function getDirFile():string{
-        return($this->dirFile);
+    public function getFileDir():string{
+        return($this->fileDir);
     }
 
     public function setFile($file){
@@ -55,22 +55,24 @@ class Files {
     }
 
     public function uploadFile(){
+        // Valida o arquivo 
         $resValid = $this->validFile();
 
         if($resValid["status"] >= 400){
             return(json_encode($resValid));
         }
 
+        // Upload com base no tipo definido
         switch ($this->getUploadType()) {
-            case 0:
+            case 0: // Upload local no servidor do PHP
                 $response = $this->uploadFileToPhpServer();
                 break;
             
-            case 1;
+            case 1; // Upload online no repositório remoto do Imgur
                 $response = $this->uploadFileToRepository();
                 break;
 
-            default:
+            default: 
                 $response = json_encode([
                     "status"=> 400, 
                     "message"=>"Opção de upload inválida.", 
@@ -79,26 +81,43 @@ class Files {
                 break;
         }
 
+        // Retorna a resposta
         return($response);
     }
 
     public function downloadFile(){
+        // Cria o diretório
         $this->createDir();
+
+        // Pega o conteúdo do link indicado e pega o nome do arquivo
         $content = file_get_contents($this->getFilePath());
         $sourceFilename = parse_url($this->getFilePath())["path"];
-        $basename = "";
 
         do {
+            // Define um novo nome para o arquivo 
             $filename = $this->makeFilename($sourceFilename);
-            $basename = $this->getDirFile().DIRECTORY_SEPARATOR.$filename;
-        } while (is_file($basename));
+            $this->setFilePath($this->getFileDir().DIRECTORY_SEPARATOR.$filename);
+        } while (is_file($this->getFilePath()));
 
-        $file = fopen($basename, "w+");
+        // Cria o array de arquivo para validação
+        $this->setFile([
+            "name"=>basename($sourceFilename),
+            "size"=>strlen($content)
+        ]);
 
+        // Valida o arquivo
+        $resValid = $this->validFile();
+
+        if($resValid["status"] >= 400){
+            return(json_encode($resValid));
+        }
+
+        // Cria o arquivo no PHP e o preenche com os dados obtidos no link
+        $file = fopen($this->getFilePath(), "w+");
         fwrite($file, $content);
-
         fclose($file);
 
+        // Retorna a resposta de sucesso
         return(json_encode([
             "status"=> 200, 
             "message"=>"Download realizado com sucesso.", 
@@ -107,12 +126,14 @@ class Files {
     }
 
     private function createDir(){
-        if(!is_dir($this->getDirFile())){
-            mkdir($this->getDirFile());
+        // Cria o diretório do arquivo caso o mesmo não exista
+        if(!is_dir($this->getFileDir())){
+            mkdir($this->getFileDir());
         }
     }
 
     private function makeFilename($sourceFilename):string{
+        // Cria um nome de arquivo aleatório, mantendo a extensão
         $newFilename = md5(uniqid());
         $extension = pathinfo($sourceFilename, PATHINFO_EXTENSION);
 
@@ -122,12 +143,13 @@ class Files {
     }
 
     private function validFile():array{
+        // Define as extensões permitidas e pega a extensão do arquivo
         $aprovedExtensions = ["png", "jpg", "jpeg", "gif", "jfif", "bmp"];
         $fileExtension = pathinfo($this->getFile()["name"], PATHINFO_EXTENSION);
 
-        if(!in_array($fileExtension, $aprovedExtensions)){
+        if(!in_array($fileExtension, $aprovedExtensions)){ // Valida se a extensão é permitida
             $response = ["status"=> 400, "title"=>"Arquivo inválido", "message"=>"Extensão do arquivo não permitida!", "items"=>[]];
-        } elseif($this->getFile()["size"] > 2097152) {
+        } elseif($this->getFile()["size"] > 2097152) { // Valida se está dentro do tamanho máximo permitido
             $response = ["status"=> 400, "title"=>"Arquivo inválido", "message"=>"Só é permitido arquivos com tamanho máximo de 2MB!", "items"=>[]];            
         } else{
             $response = ["status"=> 200, "title"=>"OK", "message"=>"Arquivo válido.", "items"=>[]];            
@@ -137,6 +159,7 @@ class Files {
     }
 
     private function uploadFileToRepository():string{  
+        // Transforma o arquivo em stream
         $handle = fopen($this->getFile()["tmp_name"], "rb");
         $imageSource = stream_get_contents($handle, filesize($this->getFile()["tmp_name"]));
     
@@ -152,7 +175,8 @@ class Files {
     
         // Executa a requisição
         $response = curl_exec($ch); 
-        
+    
+        // Valida correu tudo bem com a requisição
         if(curl_errno($ch)) {
             return(json_encode([
                 "status"=> 500, 
@@ -167,6 +191,7 @@ class Files {
 
         $response = json_decode($response, true); 
         
+        // Valida se obteve todos os dados do upload
         if(!$response["data"]["link"] || $response["data"]["link"] == ""){
             return(json_encode([
                 "status"=> 500, 
@@ -176,8 +201,10 @@ class Files {
             ]));
         }
 
+        // Armazena o link da imagem 
         $this->setFilePath($response["data"]["link"]);
 
+        // Retorna a resposta de sucesso
         return(json_encode([
             "status"=> 200, 
             "message"=>"Upload realizado com sucesso.", 
@@ -189,15 +216,19 @@ class Files {
     }
 
     private function uploadFileToPhpServer():string{ 
+        // Cria um diretório
         $this->createDir();
 
         do {
+            // Define um novo nome para o arquivo 
             $filename = $this->makeFilename($this->getFile()["name"]);
-            $this->setFilePath($this->getDirFile().DIRECTORY_SEPARATOR.$filename);
+            $this->setFilePath($this->getFileDir().DIRECTORY_SEPARATOR.$filename);
         } while (is_file($this->getFilePath()));
 
+        // Move o arquivo da temp para a pasta do servidor PHP
         $uploaded = move_uploaded_file($this->getFile()["tmp_name"], $this->getFilePath());
 
+        // Valida se ocorreu um erro
         if(!$uploaded){
             return(json_encode([
                 "status"=> 500, 
@@ -206,11 +237,12 @@ class Files {
             ]));
         }
 
+        // Retorna a resposta de sucesso
         return(json_encode([
             "status"=> 200, 
             "message"=>"Upload realizado com sucesso.", 
             "items"=>[
-                "dirFile"=>$this->getDirFile(),
+                "fileDir"=>$this->getFileDir(),
                 "file"=>$this->getFile(),
                 "filePath"=>$this->getFilePath(),
             ]
