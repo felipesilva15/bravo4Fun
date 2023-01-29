ab56bc363a928cbab8b34dfbd011225617591c5d
Felipe Silva <felipe.allware@gmail.com>
2023-01-29 13:24:02 -0300

    Desfazendo alterações de testes

--- views/menu.php
+++ views/menu.php
@@ -238,8 +238,6 @@
     ?>
   <script src="../node_modules/chart.js/dist/chart.min.js"></script>
   <script>
-    console.log('Teste do action do GitHub.');
-
     const ctx1 = document.getElementById('productsPerCategoryChart').getContext('2d');
     const productsPerCategoryChart = new Chart(ctx1, {
       type: 'doughnut',
